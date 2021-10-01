025d2113531e2e5a6c08425d34ad7b74c5fdf8f8
Matteo <matteo.pipitone93@gmail.com>
2021-10-01 10:59:32 +0200

    aggiunti test

--- challenge-one/python-solution.py
+++ challenge-one/python-solution.py
@@ -1,9 +1,21 @@
-if __name__ == "__main__":
+import unittest
 
-    arrayOfIntegers = [1, 4, 5, 9, 0, -1, 5]
 
+def count_odd(array_of_integer):
     result = 0
-    for integer in arrayOfIntegers:
+    for integer in array_of_integer:
         result += integer % 2
+    return result
 
-    print(result)
+
+class TestCountOdd(unittest.TestCase):
+
+    def test_count_odd(self):
+        arrayOfIntegers = [1, 4, 5, 9, 0, -1, 5]
+        result = count_odd(arrayOfIntegers)
+
+        self.assertEqual(result, 5)
+
+
+if __name__ == "__main__":
+    unittest.main()
