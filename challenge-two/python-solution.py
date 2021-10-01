5395809faf3da2a7ea53c52998743b9ae06f3817
Matteo <matteo.pipitone93@gmail.com>
2021-10-01 11:11:45 +0200

    challenge two

--- challenge-two/python-solution.py
+++ challenge-two/python-solution.py
@@ -1,9 +1,27 @@
+import unittest
+from datetime import datetime
+
+def detect_day_of_week(date: datetime):
+    weekend_day_number = {
+        0: 'weekend',
+        6: 'weekend'
+    }
+    return weekend_day_number.get(date.weekday(), 'weekday')
+
+
+class TestDayOfWeek(unittest.TestCase):
+    def test_day_of_week(self):
+        result = detect_day_of_week(date=datetime(2020, 5, 20))
+        self.assertEqual(result, 'weekday')
+
+    def test_day_of_weekend_sunday(self):
+        result = detect_day_of_week(date=datetime(2020, 5, 25))
+        self.assertEqual(result, 'weekend')
+
+    def test_day_of_weekend_saturday(self):
+        result = detect_day_of_week(date=datetime(2020, 5, 24))
+        self.assertEqual(result, 'weekend')
+
+
 if __name__ == "__main__":
-
-    arrayOfIntegers = [1, 4, 5, 9, 0, -1, 5]
-
-    result = 0
-    for integer in arrayOfIntegers:
-        result += integer % 2
-
-    print(result)
+    unittest.main()
